e926cd19b043dc76efb608ae21f90d25161875ed
David Fernandez <david1983xtc@gmail.com>
2016-02-23 16:07:22 +0100

    Modifies log fields

--- server/decorator/logger.go
+++ server/decorator/logger.go
@@ -23,6 +23,6 @@ func (l Logger) Do(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		t0 := time.Now()
 		h(w, r)
-		l.Logger.Printf("%s Took: %s", r.RequestURI, time.Now().Sub(t0).String())
+		l.Logger.Printf("%s %s %s %s", r.RemoteAddr, r.Method, r.RequestURI, time.Now().Sub(t0).String())
 	}
 }
